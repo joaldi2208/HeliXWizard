308759aaaf03958fa7548dc2cd85275fa242ef84
Jonas Dietrich <rantanplan@Jonass-iMac.local>
2022-11-06 20:55:20 +0100

    rewritten with functions

--- Scripts/Data-Grabber.py
+++ Scripts/Data-Grabber.py
@@ -1,7 +1,7 @@
-
+# before using the script, download the data with the following command 
 # rsync -rlpt -v -z --delete --port=33444 \
 # rsync.rcsb.org::ftp_data/structures/divided/nmr_chemical_shifts/ ../Data
-# before using the script, download the data with the command above
+
 import pycurl
 import certifi
 from io import BytesIO
@@ -10,45 +10,44 @@ import gzip
 import os
 from collections import namedtuple
 from itertools import chain
+from functools import partial
 
 from tempfile import gettempdir
 import biotite.sequence.io.genbank as gb
 import biotite.database.entrez as entrez
+import biotite
 
+import re
 # part 1
-path2files = []
-for root, _, filenames in os.walk("../Data/"):
-    path2files.extend([os.path.join(root, name) for name in filenames])
-
+def get_paths():
+    path2files = []
+    for root, _, filenames in os.walk("../Data/"):
+        path2files.extend([os.path.join(root, name) for name in filenames])
+    return path2files
 
 # part 2
-entry_ids = []
-files_not_found = 0
-for files in path2files:
-    entry_id = namedtuple("entry_id", ["pdb", "bmrb"])
-    try:
-        with gzip.open(files, "rt") as f:
-            for line in f:
-                if "_Entry.ID" in line:
-                    entry_id.pdb = line.split()[1]
-                elif "_Entry.Assigned_BMRB_ID" in line: 
-                    entry_id.bmrb = line.split()[1]
-                    if entry_id.bmrb != "1" and entry_id.bmrb != "?":
-                        entry_ids.append(entry_id) # bmrb-id is last interesting line in file
-                    break # no need to read entire file
-    except FileNotFoundError:
-        files_not_found += 1
-        #print(files)
+def get_ids(path2files):
+    entry_ids = []
+    files_not_found = 0
+    for files in path2files:
+        entry_id = namedtuple("entry_id", ["pdb", "bmrb"])
+        try:
+            with gzip.open(files, "rt") as f:
+                for line in f:
+                    if "_Entry.ID" in line:
+                        entry_id.pdb = line.split()[1]
+                    elif "_Entry.Assigned_BMRB_ID" in line: 
+                        entry_id.bmrb = line.split()[1]
+                        if entry_id.bmrb != "1" and entry_id.bmrb != "?":
+                            entry_ids.append(entry_id) # bmrb-id is last interesting line in file
+                            break # no need to read entire file
+        except FileNotFoundError:
+            files_not_found += 1
+    return entry_ids, files_not_found
 
-print(files_not_found)
-print(len(entry_ids))
 
-#https://api.bmrb.io/current/entry/34549/simulate_hsqc?format=csv&filter=backbone
-
-# part 3
-for entry in entry_ids:
-    pdb_id = entry.pdb
-    bmrb_id = entry.bmrb
+# part 4
+def get_csv_HSQC(bmrb_id):
     buffer = BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, f"https://api.bmrb.io/current/entry/{bmrb_id}/simulate_hsqc?format=csv&filter=backbone")
@@ -58,14 +57,77 @@ for entry in entry_ids:
     c.close()
     body = buffer.getvalue() # byte string
     csv_HSQC = body.decode("iso-8859-1")
-    z = 0
-    if "error" not in csv_HSQC:
-        #print(csv_HSQC)
-        z += 1
-        filename = entrez.fetch(f"{pdb_id}_A", gettempdir(), "gb", "protein", "gb")
-        gb_file = gb.GenBankFile.read(filename)
-        annotation = gb.get_annotation(gb_file, include_only=["SecStr"])
-        print(annotation)
-    
-    # TODO: How to save the data???
+    return csv_HSQC
 
+
+def get_chain_type(chain_rex, pdb_id):
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, f"https://files.rcsb.org/header/{pdb_id}.pdb")
+    c.setopt(c.WRITEDATA, buffer)
+    c.setopt(c.CAINFO, certifi.where())
+    c.perform()
+    c.close()
+    body = buffer.getvalue() # byte string
+    pdb_header = body.decode("iso-8859-1")
+    
+    chain_type = chain_rex.search(str(pdb_header)).group(1)
+    return chain_type
+
+
+def get_2nd_str_data(loc_rex, len_rex, type_rex, gb_file):
+    sec_str_loc = loc_rex.findall(str(gb_file))
+    sec_str_type = type_rex.findall(str(gb_file))
+    sec_str_len = len_rex.findall(str(gb_file))[1]
+    if sec_str_len != None:
+        return sec_str_loc, sec_str_type, sec_str_len
+     
+
+def get_data(entry_ids, get_chain_type_rex, get_2nd_str_data_rex):
+    # you should save the data here in a list and then return the list
+    data = []
+    files_not_found = 0
+    for entry_id in entry_ids:
+        csv_HSQC = get_csv_HSQC(entry_id.bmrb)
+        chain_type = get_chain_type_rex(entry_id.pdb)
+        if "error" not in csv_HSQC:
+            try:
+                filename = entrez.fetch(f"{entry_id.pdb}_{chain_type}", gettempdir(), "gb", "protein", "gb")
+                gb_file = gb.GenBankFile.read(filename)
+                sec_str_loc, sec_str_type, sec_str_len = get_2nd_str_data_rex(gb_file)
+            except biotite.database.RequestError:
+                files_not_found += 1
+            finally:
+                data.append((entry_id, chain_type, sec_str_loc, sec_str_type, sec_str_len))
+                print("n: ", len(data))
+                print("pdb_id: ", entry_id.pdb)
+                print("bmrb_id: ", entry_id.bmrb)
+                print("chain type: ", chain_type)
+                print("sec_str_loc: ", sec_str_loc)
+                print("sec_str_type: ", sec_str_type)
+                print("sec_str_len: ", sec_str_len)
+                print("files_not_found: ", files_not_found)
+                print("------------------------------------------------")
+               
+    return data
+        
+
+if __name__ == "__main__":
+    chain_type_rex = re.compile("CHAIN:\ (\w)")
+    loc_rex = re.compile("SecStr\ *(\d*)\.\.(\d*)")
+    len_rex = re.compile("source\ *\d*..(\d*)")
+    type_rex = re.compile("sec_str_type=\"([^\"]*)")
+
+    get_chain_type_rex = partial(get_chain_type, chain_type_rex)
+    get_2nd_str_data_rex = partial(get_2nd_str_data, loc_rex, len_rex, type_rex)
+    
+    path2files = get_paths() # from data dir, should be changed later
+    entry_ids, path_files_not_found = get_ids(path2files)
+    print("number entry_ids: ", len(entry_ids))
+    print("path_files_not_found: ", path_files_not_found)
+    print("++++++++++++++++++++++++++++++++++++++++++")
+    
+    data = get_data(entry_ids, get_chain_type_rex, get_2nd_str_data_rex)
+    # what about pdb_id:  6V1N, https://www.ncbi.nlm.nih.gov/Structure/pdb/6V1N
+    print("++++++++++++++++++++++++++++++++++++++++++")
+    print(len(data))
