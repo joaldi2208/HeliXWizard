686c165686400428847dd79a89b5c5456eeca36b
Alexander Dietrich <alexanderdietrich@Jonass-MacBook-Pro.local>
2022-11-30 12:58:12 +0100

    update preprocessing module and remove IDs with more then one shift table

--- Scripts/Data-Generator/Data-Grabber2.py
+++ Scripts/Data-Generator/Data-Grabber2.py
@@ -44,8 +44,8 @@ def get_data(path2files, get_pdb_id_rex, get_chemical_shifts_rex, get_sec_struc_
     for i, files in enumerate(path2files):
         try:
             with gzip.open(files, "rt") as f:
-                if i in list(range(50,6000,50)):
-                    print(i)
+                ####if i in list(range(50,6000,50)):
+                print(i)
                 nmrstar_file = f.read()
                 chemical_shifts_short = get_chemical_shifts_rex(nmrstar_file)
                 if type(chemical_shifts_short) != str:
@@ -76,82 +76,6 @@ def get_data(path2files, get_pdb_id_rex, get_chemical_shifts_rex, get_sec_struc_
 
 
 
-def get_chemical_shifts(headers_rex, nmrstar_file):
-    """ids"""
-    #def is_protein(chemical_shifts):
-    #    """RNA has a one letter id while proteins have three"""
-    #    amino_acid_code = [
-    #        "ALA",
-    #        "ASP",
-    #        "CYS",
-    #        "GLU",
-    #        "PHE",
-    #        "GLY",
-    #        "HIS",
-    #        "ILE",
-    #        "LYS",
-    #        "LEU",
-    #        "MET",
-    #        "ASN",
-    #        "PRO",
-    #        "GLN",
-    #        "ARG",
-    #        "SER",
-    #        "THR",
-    #        "VAL",
-    #        "TRP",
-    #        "TYR"
-    #        ]
-    #    if chemical_shifts["Comp_ID"][0] in amino_acid_code:
-    #        return True
-        
-    def create_dataframe(last_header, nmrstar_file, n):
-        chemical_shifts_range = nmrstar_file[(nmrstar_file.find(last_header, nmrstar_file.find(last_header)+n)+len(last_header)) : ]
-        chemical_shifts_range = chemical_shifts_range[: (chemical_shifts_range.find("stop_"))]
-        stream = io.StringIO()
-        stream.write(chemical_shifts_range)
-        stream.seek(0)
-        chemical_shifts = pd.read_csv(stream, delim_whitespace=True, names=unique_headers, quotechar="'", dtype={"Entry_ID": object})
-        stream.close()
-        length_comp_id = chemical_shifts.Comp_ID.str.len()
-        if length_comp_id.min() == 3:
-            return chemical_shifts
-        else:
-            return pd.DataFrame(columns=unique_headers) # Protein Error
-        
-
-    headers = headers_rex.findall(nmrstar_file)
-    if headers:
-        df_in_file = []
-        unique_headers = list(OrderedSet(headers))
-        runtimes = len(headers) / len(unique_headers)
-        last_header = headers[-1]
-        for n in range(int(runtimes)):
-            chemical_shifts = create_dataframe(last_header, nmrstar_file, n)
-            df_in_file.append(chemical_shifts)
-        if len(df_in_file) > 1: ###
-            print(nmrstar_file[:9]) ###
-            n_rows = [df.shape[0] for df in df_in_file]
-            max_n_row = max(n_rows)
-            index_max_rows = n_rows.index(max_n_row)
-            df_in_file = df_in_file.append(df_in_file[index_max_rows])
-        ##if df_in_file:
-            ##combined_chemical_shifts = pd.concat(df_in_file)
-        headers_of_interest = ["Comp_index_ID","Comp_ID","Atom_ID","Atom_type","Val","Entry_ID"]
-        chemical_shifts_short = chemical_shifts[chemical_shifts.columns.intersection(headers_of_interest)]
-        if chemical_shifts_short.shape[0] != 0:
-            if "Entry_ID" in chemical_shifts_short.columns:
-                return chemical_shifts_short
-            else:
-                return "Entry_ID Error"
-        else:
-            return "Protein Error"
-        #else:
-        #    return "Protein Error"
-    else:
-        return "Column Header Error"
-    
-    
 def get_pdb_id(pdb_id_rex, nmrstar_file):
     pdb_id = pdb_id_rex.search(nmrstar_file)
     try:
@@ -160,47 +84,71 @@ def get_pdb_id(pdb_id_rex, nmrstar_file):
         return "PDB ID Error"
 
 
+def from_list2table(cond_struc_depot):
+    headers = ["Entry_ID","H","B","E","G","I","T","S","Temp","PH","Ionic_strength","Pressure","Seq_length"]
+    cond_struc_table = pd.DataFrame(cond_struc_depot, columns = headers)
+    return cond_struc_table
+
+#########################################################
+#                   CHEMICAL SHIFTS
+#########################################################
+
+def get_chemical_shifts(headers_rex, nmrstar_file):
+    """ids"""
+    headers = headers_rex.findall(nmrstar_file)
+    if headers:
+        df_in_file = []
+        unique_headers = list(OrderedSet(headers))
+        runtimes = len(headers) / len(unique_headers)
+        last_header = headers[-1]
+        for n in range(int(runtimes)):
+            chemical_shifts = create_dataframe(last_header, nmrstar_file, n, unique_headers)
+            df_in_file.append(chemical_shifts)
+        if len(df_in_file) > 1: ###
+            for i in df_in_file:
+                print(i.shape[0])
+            print(nmrstar_file[:9]) ###
+            n_rows = [df.shape[0] for df in df_in_file]
+            max_n_row = max(n_rows)
+            index_max_rows = n_rows.index(max_n_row)
+            df_in_file = df_in_file.append(df_in_file[index_max_rows])
+        ##if df_in_file:
+            ##combined_chemical_shifts = pd.concat(df_in_file)
+        else:
+            headers_of_interest = ["Comp_index_ID","Comp_ID","Atom_ID","Atom_type","Val","Entry_ID"]
+            chemical_shifts_short = chemical_shifts[chemical_shifts.columns.intersection(headers_of_interest)]
+        ##if chemical_shifts_short.shape[0] != 0:
+            if "Entry_ID" in chemical_shifts_short.columns:
+                return chemical_shifts_short
+            else:
+                return "Entry_ID Error"
+    else:
+        return "Column Header Error"
+        
+def create_dataframe(last_header, nmrstar_file, n, unique_headers):
+	chemical_shifts_range = nmrstar_file[(nmrstar_file.find(last_header, nmrstar_file.find(last_header)+n)+len(last_header)) : ]
+	chemical_shifts_range = chemical_shifts_range[: (chemical_shifts_range.find("stop_"))]
+	stream = io.StringIO()
+	stream.write(chemical_shifts_range)
+	stream.seek(0)
+	chemical_shifts = pd.read_csv(stream, delim_whitespace=True, names=unique_headers, quotechar="'", dtype={"Entry_ID": object})
+	stream.close()
+	length_comp_id = chemical_shifts.Comp_ID.str.len()
+	if length_comp_id.min() == 3:
+	    return chemical_shifts
+	else:
+	    return pd.DataFrame(columns=unique_headers) # Protein Error
+        
+
+    
+    
+
+#########################################################
+#                   SECONDARY STRUCTURE
+#########################################################
+
 def get_sec_struc(sec_struc_rex, pdb_id):
     """dssp"""
-    def load_dssp(pdb_id):
-        """loads the dssp file from the api with the pdb id"""
-        formdata = {"data": pdb_id}
-        try:
-            url_create = ("https://www3.cmbi.umcn.nl/xssp/api/create/pdb_id/dssp/")
-            r = requests.post(url_create, data=formdata)
-            r.raise_for_status()
-            job_id = json.loads(r.text)['id']
-            ready = False
-            while not ready:
-                url_status = ("https://www3.cmbi.umcn.nl/xssp/api/status/pdb_id/dssp/{}/".format(job_id))
-                r = requests.get(url_status)
-                r.raise_for_status()
-                status = json.loads(r.text)['status']
-                if status == "SUCCESS":
-                    ready = True
-                elif status in ["FAILURE", "REVOKED"]:
-                    #raise Exception(json.loads(r.text)["message"])
-                    return "DSSP Error"
-                else:
-                    time.sleep(2)
-            else:  
-                url_result = ("https://www3.cmbi.umcn.nl/xssp/api/result/pdb_id/dssp/{}/".format(job_id))
-                r = requests.get(url_result)
-                r.raise_for_status()
-                result = json.loads(r.text)['result']
-                return result
-
-        except requests.exceptions.HTTPError:
-            return "DSSP Error"
-
-        
-    def get_sec_struc_counts(sec_struc):
-        """counts the appearance of symbols in sec struc list"""
-        c = Counter(sec_struc)
-        avail_sec_struc = ["H","B","E","G","I","T","S"]
-        counts_in_order = [ c[sym] for sym in avail_sec_struc ]
-        return counts_in_order
-
     dssp_file = load_dssp(pdb_id)
     if dssp_file != "DSSP Error":
         sec_struc = sec_struc_rex.findall(dssp_file)
@@ -210,55 +158,53 @@ def get_sec_struc(sec_struc_rex, pdb_id):
         return ["DSSP Error"], "_"
 
 
+def load_dssp(pdb_id):
+    """loads the dssp file from the api with the pdb id"""
+    formdata = {"data": pdb_id}
+    try:
+        url_create = ("https://www3.cmbi.umcn.nl/xssp/api/create/pdb_id/dssp/")
+        r = requests.post(url_create, data=formdata)
+        r.raise_for_status()
+        job_id = json.loads(r.text)['id']
+        ready = False
+        while not ready:
+            print("not ready")
+            url_status = ("https://www3.cmbi.umcn.nl/xssp/api/status/pdb_id/dssp/{}/".format(job_id))
+            r = requests.get(url_status)
+            r.raise_for_status()
+            status = json.loads(r.text)['status']
+            if status == "SUCCESS":
+                ready = True
+            elif status in ["FAILURE", "REVOKED"]:
+                #raise Exception(json.loads(r.text)["message"])
+                return "DSSP Error"
+            else:
+                time.sleep(2)
+        else:  
+            url_result = ("https://www3.cmbi.umcn.nl/xssp/api/result/pdb_id/dssp/{}/".format(job_id))
+            r = requests.get(url_result)
+            r.raise_for_status()
+            result = json.loads(r.text)['result']
+            return result
+
+    except requests.exceptions.HTTPError:
+        return "DSSP Error"
+
+        
+def get_sec_struc_counts(sec_struc):
+    """counts the appearance of symbols in sec struc list"""
+    c = Counter(sec_struc)
+    avail_sec_struc = ["H","B","E","G","I","T","S"]
+    counts_in_order = [ c[sym] for sym in avail_sec_struc ]
+    return counts_in_order
+
+
+#########################################################
+#                    CONDITIONS
+#########################################################
+
 def get_cond(pdb_id):
     """conditions"""
-    
-    def load_pdb_header(pdb_id):
-        """loads the pbd header in a buffer"""
-        buffer = io.BytesIO()
-        c = pycurl.Curl()
-        c.setopt(c.URL, f"https://files.rcsb.org/header/{pdb_id}.pdb")
-        c.setopt(c.WRITEDATA, buffer)
-        c.setopt(c.CAINFO, certifi.where())
-        c.perform()
-        c.close()
-        body = buffer.getvalue() # byte string
-        pdb_header = body.decode("iso-8859-1")
-        return pdb_header
-    
-    def find_cond(pdb_header_lines):
-        """find the values for the condition parameters"""
-        cond_ensemble = {}
-        cond_types = ["Temp", "PH", "Ionic_Strengh", "Pressure"]
-        catch_count = 0
-        line_count = 0
-        line = pdb_header_lines[line_count] #here is a mistake, maybe empty file after 2900
-        catch = False
-        while "SAMPLE CONTENTS" not in line:
-            if catch == True:
-                values = line[delimiter_location+1 :]
-                if ":" in line:
-                    cond = {cond_types[catch_count] : values.strip()}
-                    cond_ensemble.update(cond)
-                    catch_count += 1
-                else: 
-                    cond_ensemble[cond_types[catch_count-1]] += values.strip()
-            if "EXPERIMENT TYPE" in line and "NMR" in line:
-                catch = True
-                delimiter_location = line.find(":")
-            line_count += 1
-            line = pdb_header_lines[line_count]
-        return cond_ensemble
-
-    def make_iterable(pdb_header):
-        """returns file like object to iterate through"""
-        stream = io.StringIO()
-        stream.write(pdb_header)
-        stream.seek(0)
-        pdb_header_lines = stream.readlines()
-        stream.close()
-        return pdb_header_lines
-        
     pdb_header = load_pdb_header(pdb_id)
     if "The requested URL was not found on this server" in pdb_header:
         return "PDB Request Error"
@@ -266,11 +212,54 @@ def get_cond(pdb_id):
         pdb_header_lines = make_iterable(pdb_header)
         cond = find_cond(pdb_header_lines)
         return cond
+    
+def load_pdb_header(pdb_id):
+    """loads the pbd header in a buffer"""
+    buffer = io.BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, f"https://files.rcsb.org/header/{pdb_id}.pdb")
+    c.setopt(c.WRITEDATA, buffer)
+    c.setopt(c.CAINFO, certifi.where())
+    c.perform()
+    c.close()
+    body = buffer.getvalue() # byte string
+    pdb_header = body.decode("iso-8859-1")
+    return pdb_header
 
-def from_list2table(cond_struc_depot):
-    headers = ["Entry_ID","H","B","E","G","I","T","S","Temp","PH","Ionic_strength","Pressure","Seq_length"]
-    cond_struc_table = pd.DataFrame(cond_struc_depot, columns = headers)
-    return cond_struc_table
+def find_cond(pdb_header_lines):
+    """find the values for the condition parameters"""
+    cond_ensemble = {}
+    cond_types = ["Temp", "PH", "Ionic_Strengh", "Pressure"]
+    catch_count = 0
+    line_count = 0
+    line = pdb_header_lines[line_count]
+    catch = False
+    while "SAMPLE CONTENTS" not in line:
+        if catch == True:
+            values = line[delimiter_location+1 :]
+            if ":" in line:
+                cond = {cond_types[catch_count] : values.strip()}
+                cond_ensemble.update(cond)
+                catch_count += 1
+            else: 
+                cond_ensemble[cond_types[catch_count-1]] += values.strip()
+        if "EXPERIMENT TYPE" in line and "NMR" in line:
+            catch = True
+            delimiter_location = line.find(":")
+        line_count += 1
+        line = pdb_header_lines[line_count]
+    return cond_ensemble
+
+def make_iterable(pdb_header):
+    """returns file like object to iterate through"""
+    stream = io.StringIO()
+    stream.write(pdb_header)
+    stream.seek(0)
+    pdb_header_lines = stream.readlines()
+    stream.close()
+    return pdb_header_lines
+        
+
 
                 
 if __name__ == "__main__":
--- Scripts/Data-Generator/Data-Preprocessor.py
+++ Scripts/Data-Generator/Data-Preprocessor.py
@@ -4,7 +4,13 @@ import re
 from functools import partial
 import numpy as np
 
-# write functions for one entry and use the lambda apply function
+
+
+
+###########################################
+#        GENERAL FUNCTIONS
+###########################################
+
 def remove_spaces(entry):
     return entry.replace(" ","")
 
@@ -13,72 +19,115 @@ def eliminate_redundancy(entry):
     unique_values = ",".join(set(values))
     return unique_values
 
+
+
+###########################################
+#       PRESSURE TRANSFORMATION
+###########################################
+
 def uniform_pressure(sep_val_unit_rex, pressure):
     """hnt"""
-    def determine_unit(sep_val_unit_rex, pressure):
-        units = ["TORR", "ATM", "PA", "MMHG", "BAR", "MBAR"]
-        match = sep_val_unit_rex.search(pressure)
-        return match.groups()
-    def remove_words(unit):
-        words_to_remove = ["AMBIENT", "ATMOSPHERIC", "VARIABLE"]
-        for word in words_to_remove:
-             unit = unit.replace(word, "")
-        #if unit == "MBARMBAR":
-        #    unit = unit.replace("MBARMBAR", "MBAR")
-        #elif unit == "PAATM":
-        #    unit = unit.replace("PAATM", "PA")
-        return unit
-    def pressure2pascal(value, unit):
-        if unit == "MBAR":
-            return float(value) * 100
-        elif unit == "BAR":
-            return float(value) * 100000
-        elif unit == "ATM":
-            return float(value) * 101325
-        elif unit == "MMHG":
-            return float(value) * 133.322
-        elif unit == "TORR":
-            return float(value) * 133.322
-        elif unit == "PA":
-            return float(value)
-        else:
-            return -1          
     value, unit = determine_unit(sep_val_unit_rex, pressure)
     if value != None and unit != None:
         only_unit = remove_words(unit)
         p_in_pa = pressure2pascal(value, only_unit)
         #print(p_in_pa)
-        if p_in_pa < 100000 or p_in_pa > 101325:
-            print(p_in_pa, value, unit)
+        #if p_in_pa < 100000 or p_in_pa > 101325:
+            #print(p_in_pa, value, unit)
     return value, unit
+
+def determine_unit(sep_val_unit_rex, pressure):
+    units = ["TORR", "ATM", "PA", "MMHG", "BAR", "MBAR"]
+    match = sep_val_unit_rex.search(pressure)
+    return match.groups()
+
+def remove_words(unit):
+    words_to_remove = ["AMBIENT", "ATMOSPHERIC", "VARIABLE"] # check variable value in literature
+    for word in words_to_remove:
+        unit = unit.replace(word, "101.325")
+    if unit == "MBARMBAR":
+        unit = unit.replace("MBARMBAR", "MBAR")
+    elif unit == "PAATM":
+        unit = unit.replace("PAATM", "PA")
+    return unit
+
+def pressure2pascal(value, unit):
+    if unit == "MBAR":
+       return float(value) * 100
+    elif unit == "BAR":
+       return float(value) * 100000
+    elif unit == "ATM":
+       return float(value) * 101325
+    elif unit == "MMHG":
+       return float(value) * 133.322
+    elif unit == "TORR":
+       return float(value) * 133.322
+    elif unit == "PA":
+       return float(value)
+    else:
+      return -1          
 
 def replace_words():
     pass
 
-def pressure2pascal():
-    pass
     
 
-def calc_mean_value(entry):
-    try:
-        value1, value2 = entry.split("-")
-        mean_value = ( float(value1) + float(value2) ) / 2
-        return mean_value
-    except:
-        return entry
         
+###########################################
+#       PH, TEMPERATURE TRANSFORMATION
+###########################################
     
+def calc_mean_values(entry):
+    splitted_entries = entry.split(",")
+    while "NULL" in splitted_entries:
+        splitted_entries.remove("NULL")
+    if "-" in entry:
+        entries = []
+        for value in splitted_entries:
+            if "-" in value:
+                mean_range = calc_mean_range(value)
+                entries.append(mean_range)            
+            else:  	        
+                entries.append(value)
+        return np.mean(list(map(float, entries)))
+    elif len(splitted_entries) > 1:
+        return np.mean(list(map(float, splitted_entries)))
+    else:
+        return entry
+	
+	    	 
+def calc_mean_range(entry):
+    value1, value2 = entry.split("-")
+    mean_range = ( float(value1) + float(value2) ) / 2
+    return mean_range
+
+def convert2float(entry):
+    try:
+        float_entry = float(entry)
+        return float_entry
+    except ValueError:
+        mean_values = calc_mean_values(entry)
+        return mean_values
+
+
+
 
 if __name__== "__main__":
     sep_val_unit_rex = re.compile("([\d.]+)?([A-Z]+)?")
     uniform_pressure_rex = partial(uniform_pressure, sep_val_unit_rex)
-    #chemical_shifts = pd.read_pickle("chemical-shifts.pkl")
-    struc_and_cond = pd.read_pickle("struc-and-cond.pkl")
+    #chemical_shifts = pd.read_pickle("chemical-shifts.pkl.gz",compression="gzip")
+    struc_and_cond = pd.read_pickle("struc-and-cond.pkl.gz",compression="gzip")
     struc_and_cond = struc_and_cond.fillna(value="NONE")
-    parameters = ["Temp", "Pressure", "PH", "Ionic_strength"]
+    #parameters = ["Temp", "Pressure", "PH", "Ionic_strength"]
+    parameters = ["Pressure"]
     for para in parameters:
         para_no_spaces = struc_and_cond[para].apply(lambda x: remove_spaces(x))
         para_no_double = para_no_spaces.apply(lambda x: eliminate_redundancy(x))
         print(para_no_double)
-    #pressure_in_pa = para_no_double.apply(lambda x: uniform_pressure_rex(x))
-    
+    pressure_in_pa = para_no_double.apply(lambda x: uniform_pressure_rex(x))
+    #print(pressure_in_pa)
+    for i in pressure_in_pa:
+        print(i)
+    #entries_converted = para_no_double.apply(lambda x: convert2float(x))
+    #for i in entries_converted:
+    #    print(i)
